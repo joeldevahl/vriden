53730314cc33420ecce96253550522ad8b319ba9
Joel de Vahl <joel@devahl.com>
2017-02-22 17:24:17 +0100

    Minor warning fix to file

--- units/foundation/include/foundation/file.h
+++ units/foundation/include/foundation/file.h
@@ -14,11 +14,12 @@ enum file_seek_mode_t
 };
 
 struct file_t;
+struct allocator_t;
 
 file_t* file_open(const char* file_name, file_open_mode_t mode);
 void file_close(file_t* file);
 size_t file_read(file_t* file, void* dst, size_t num_bytes);
 void file_write(file_t* file, const void* src, size_t num_bytes);
 size_t file_size(file_t* file);
-void* file_open_read_all(const char* file_name, struct allocator_s* alloc, size_t* out_size);
-char* file_open_read_all_text(const char* file_name, struct allocator_s* alloc);
+void* file_open_read_all(const char* file_name, allocator_t* alloc, size_t* out_size);
+char* file_open_read_all_text(const char* file_name, allocator_t* alloc);
